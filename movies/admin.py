915fbe4809101d58380be1eed278e725d8d21859
nastya2508 <nastusiaok.2203@gmail.com>
2026-04-05 23:42:52 +0300

    Додано моделі Movie, Genre, Actor та налаштовано адмінку

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Movie, Genre, Actor
 
-# Register your models here.
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('title', 'release_year', 'get_genres')
+    search_fields = ('title', 'description')
+    list_filter = ('release_year', 'genres')
+    filter_horizontal = ('genres', 'actors') 
+
+    def get_genres(self, obj):
+        return ", ".join([g.name for g in obj.genres.all()])
+    get_genres.short_description = 'Жанри'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'birth_date')
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Назва жанру")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Жанр"
+        verbose_name_plural = "Жанри"
+
+
+class Actor(models.Model):
+    full_name = models.CharField(max_length=255, verbose_name="Ім'я та прізвище")
+    bio = models.TextField(blank=True, verbose_name="Біографія")
+    birth_date = models.DateField(null=True, blank=True, verbose_name="Дата народження")
+
+    def __str__(self):
+        return self.full_name
+
+    class Meta:
+        verbose_name = "Актор"
+        verbose_name_plural = "Актори"
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=255, verbose_name="Назва фільму")
+    description = models.TextField(verbose_name="Опис")
+    release_year = models.PositiveIntegerField(verbose_name="Рік випуску")
+    
+    # Зв'язки ManyToMany
+    genres = models.ManyToManyField(Genre, related_name="movies", verbose_name="Жанри")
+    actors = models.ManyToManyField(Actor, related_name="movies", verbose_name="Актори")
+    
+    poster = models.ImageField(upload_to='posters/', null=True, blank=True, verbose_name="Постер")
+
+    def __str__(self):
+        return f"{self.title} ({self.release_year})"
+
+    class Meta:
+        verbose_name = "Фільм"
+        verbose_name_plural = "Фільми"
